c49ffca0c7949e50fa396656eddc853bc8cb0763
Maciej Miś <macmisdev@gmail.com>
2014-10-19 14:43:43 +0200

    userController

---
+++ src/main/java/me/ksiazka/controller/userController.java
@@ -0,0 +1,21 @@
+package me.ksiazka.controller;
+
+import me.ksiazka.model.User;
+import org.springframework.ui.Model;
+
+
+public interface userController {
+    public String showUserPage(Long id, Model model);
+    public String editUserData(Long id, Model model);
+    public String updateEditedUser(User user);
+    /* -- Prawdopodobnie zrobimy to w przeływach (Web Flow)
+    public String addBookToWantList(Model model);
+    public String delBookFromWantList(Model model);
+    public String addBookToHaveList(Model model);
+    public String delBookFromHaveList(Model model);
+    public String offerTrade();
+    */
+    public String deleteAccount(Model model);
+    public String acceptTrade(Long tradeId);
+    public String declineTrade(Long tradeId);
+}
