c9cb1a347afec22bdb11734212357ef4122914a4
Maciej Miś <macmisdev@gmail.com>
2014-10-19 14:54:10 +0200

    bookController interface added

--- src/main/java/me/ksiazka/controller/bookController.java
+++ src/main/java/me/ksiazka/controller/bookController.java
@@ -1,5 +1,11 @@
 package me.ksiazka.controller;
 
 
+import me.ksiazka.model.Book;
+import org.springframework.ui.Model;
+
 public interface bookController {
+    public String addBookToSystem(Model model);
+    public String addedBookToSystem(Book book);
+    public String deleteBook(Long bookId);
 }
