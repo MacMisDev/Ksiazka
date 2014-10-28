cf6caa2aba3ae2f1de01ddab670769bb1a593c4f
Łukasz Skarżyński <LukSkarDev@gmail.com>
2014-10-28 15:10:55 +0100

    Travis - db.properties try 1

---
+++ travis-db-prop-populate.sh
@@ -0,0 +1,4 @@
+touch /src/main/webapp/db.properties
+echo db.url=jdbc:mysql://localhost:3306/myapp_test >> /src/main/webapp/db.properties
+echo db.username=root >> /src/main/webapp/db.properties
+echo db.password= >> /src/main/webapp/db.properties
