6460f40f02819cbcf59f92d376876c89890ec864
Maciej Miś <macmisdev@gmail.com>
2014-11-01 14:59:08 +0100

    Hot fix

--- src/main/java/me/ksiazka/controller/HomeControllerImpl.java
+++ src/main/java/me/ksiazka/controller/HomeControllerImpl.java
@@ -1,9 +1,11 @@
 package me.ksiazka.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
 public class HomeControllerImpl implements HomeController{
 
     @RequestMapping(value = {"/", "home"}, method = RequestMethod.GET)
