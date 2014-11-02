b6b4d644d987e008a59600f693926953d047b37e
Maciej Miś <macmisdev@gmail.com>
2014-11-02 16:42:21 +0100

    Some configuration for OfferController

--- src/main/java/me/ksiazka/controller/OfferControllerImpl.java
+++ src/main/java/me/ksiazka/controller/OfferControllerImpl.java
@@ -3,7 +3,11 @@ package me.ksiazka.controller;
 
 import me.ksiazka.service.OfferService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller("offerController")
+@RequestMapping(value = "offer")
 public class OfferControllerImpl implements OfferController {
 
     @Autowired
