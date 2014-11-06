495b3bbd27db8c5760d83339f97017de3ac8c3d6
Maciej Miś <macmisdev@gmail.com>
2014-11-06 22:39:57 +0100

    Merge branch 'master' of https://github.com/MacMisDev/Ksiazka
    Conflicts:
    	src/main/java/me/ksiazka/controller/BookControllerImpl.java
    	src/main/java/me/ksiazka/service/BookServiceImpl.java

--- src/main/java/me/ksiazka/controller/BookControllerImpl.java
+++ src/main/java/me/ksiazka/controller/BookControllerImpl.java
@@ -1,15 +1,12 @@
 package me.ksiazka.controller;
 
 import me.ksiazka.model.Book;
-import me.ksiazka.model.BookStatus;
 import me.ksiazka.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.awt.*;
 
 @Controller("bookController")
 @RequestMapping("/book")
