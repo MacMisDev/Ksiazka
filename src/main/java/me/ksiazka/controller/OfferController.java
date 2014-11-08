02634d64eba41c250447fd9f4495bd588941eb65
Maciej Miś <macmisdev@gmail.com>
2014-11-08 20:26:06 +0100

    Basic methods in offerController interface

--- src/main/java/me/ksiazka/controller/OfferController.java
+++ src/main/java/me/ksiazka/controller/OfferController.java
@@ -1,4 +1,11 @@
 package me.ksiazka.controller;
 
+import me.ksiazka.model.Offer;
+import org.springframework.ui.Model;
+
 public interface OfferController {
+    public String showOfferHome();
+    public Offer showOffer(Long id);
+    public String newOffer(Model model);
+    public Offer newOffer(Offer offer);
 }
